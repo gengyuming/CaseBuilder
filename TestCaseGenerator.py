a12888236e9ec4a7addbf3ab0a8e15fb77c41bb6
gengyuming <g13050048817>
2021-07-19 16:47:00 +0800

    first commit

--- TestCaseGenerator.py
+++ TestCaseGenerator.py
@@ -1,65 +1,89 @@
 import csv
+import codecs
+
 from XmindReader import XmindReader
 
 
 class TestCaseGenerator:
-    def __init__(self, csv_path):
-        self.csv_file = open(csv_path, 'w', newline='')
-        self.__headers__ = ['所属模块', '用例标题', '前置条件', '步骤', '预期', '关键词', '优先级', '用例类型', '适用阶段']
-        self.__csv_writer__ = csv.DictWriter(self.csv_file, self.__headers__)
-        self.__csv_writer__.writeheader()
-        self._titles = []
-        self._csv_data = []
+    def __init__(self, file_path, file_type='csv'):
 
-    def get_headers(self):
-        return self.__headers__
+        self.headers = [
+            'Name',  # 用例名称
+            'Precondition',  # 前置条件
+            'Objective',  # 页面
+            'Test Script (Steps) - Step',  # 测试脚本-步骤
+            'Test Script (Steps) - Expected Result',  # 测试脚本-期待结果
+            'Folder',  # 目录
+            'Status',  # 状态
+            'Priority',  # 优先级
+            'Component',  # 组件
+            'Labels',  # 标签
+            'Owner'  # 创建者
+        ]
 
-    def init_csv_data(self, test_data):
-        titles = []
+        self.status = ['Draft', 'Deprecated', 'Approved']
 
+        self.file_path = file_path
+
+        if file_type.lower() == 'csv':
+            self.csv_file = open(self.file_path, 'w', newline='')
+            self.__csv_writer__ = csv.writer(self.csv_file)
+            self.__csv_writer__.writerow(self.headers)
+            self.csv_data = []
+
+    def set_headers(self, headers):
+        self.headers = headers
+        return self.headers
+
+    def init_csv_data(self, test_data, folder, status, component, owner):
+        index = 1
         for data in test_data:
-            steps = data['test_case'].split('&-&')
+            test_case = data['test_case'].split('&-&')
+            name = test_case[0] + str(index).zfill(4)
+            precondition = data['precondition']
+            objective = test_case[1]
+            step = []
+            try:
+                step = test_case[2:-1]
+            except Exception as e:
+                pass
+            step = '-'.join(step)
+            expect = test_case[-1]
+            if status not in self.status:
+                status = self.status[0]
+            priority = data['priority']
+            label = data['label']
 
-            title = '_'.join(steps[:-2])
-            # index = 1
-            # while title + '_' + str(index) in titles:
-            #     index += 1
+            self.csv_data.append([name, precondition, objective, step, expect, folder, status, priority, component, label, owner])
+            index += 1
+        print(self.csv_data)
+        self.import_csv_data()
 
-            test_case = steps[-2]
+    def import_csv_data(self):
+        if self.csv_data:
+            for data in self.csv_data:
+                gbk_data = []
+                for item in data:
+                    gbk_data.append(item.encode('gbk', 'replace').decode('gbk', 'replace'))
 
-            # self._titles.append(title + '_' + str(index))
-            self._titles.append(title)
-            self._csv_data.append({'title': title,
-                                   'condition': data['condition'],
-                                   'test_case': test_case,
-                                   'expect': steps[-1],
-                                   'level': data['level']})
+                self.__csv_writer__.writerow(gbk_data)
 
-    def import_csv_data(self, module, case_type, phase):
-        if self._csv_data:
-            for data in self._csv_data:
-                row = {
-                    '所属模块': module,
-                    '用例标题': data['title'],
-                    '前置条件': data['condition'],
-                    '步骤': data['test_case'],
-                    '预期': data['expect'],
-                    '关键词': '',
-                    '优先级': data['level'],
-                    '用例类型': case_type,
-                    '适用阶段': phase
-                }
-
-                self.__csv_writer__.writerow(row)
+            print('csv数据导入成功')
         else:
             print('缺失数据，请进行数据初始化后再重新执行！')
 
 
 if __name__ == '__main__':
-    xmind_path = './ZentaoCaseBuilder/Xmind/aaa.xmind'
-    csv_save_path = './ZentaoCaseBuilder/TestCase/test_case.csv'
+    # xmind_path = 'D:\workspace\ZAGitlab\ZaTools\ZACaseBuilder\Xmind\调试脑图.xmind'
+    xmind_path = 'D:\workspace\ZAGitlab\ZaTools\ZACaseBuilder\Xmind\通知センターTest.xmind'
+    csv_save_path = 'D:\workspace\ZAGitlab\ZaTools\ZACaseBuilder\TestCase\CaseDemo.csv'
     xm = XmindReader(xmind_path)
     test_data_list = xm.get_test_data()
+
     tc = TestCaseGenerator(csv_save_path)
-    tc.init_csv_data(test_data_list)
-    tc.import_csv_data('模块', '用例类型', '适用阶段')
+    tc.init_csv_data(test_data_list,
+                     folder='目录1/目录2',
+                     status='ab',
+                     component='组件',
+                     owner='耿裕明')
+
--- XmindReader.py
+++ XmindReader.py
@@ -1,42 +1,183 @@
 import os
 import zipfile
 import json
+import xmind
 
 
 class XmindReader:
     """
         Xmind解析器
     """
-    def __init__(self, file_path):
-        self.connector = '_'
-        self.__zip_file__ = zipfile.ZipFile(file_path, 'r')
-        self._content_json = json.loads(self.__zip_file__.read('content.json').decode())
-        self._catalog = self.generate_catalog(self._content_json[0]['rootTopic'], [])
-        self._test_data = []
-        self._test_case = []
-        self.__generate_test_data__(self._catalog)
 
-    def generate_catalog(self, content_json, catalog):
+    def __init__(self, file):
+        self.connector = '&-&'
+        self.__zip_file__ = zipfile.ZipFile(file, 'r')
+        self.content_json = json.loads(self.__zip_file__.read('content.json').decode())
+        self.sheet = self.get_sheet(0)
+        self.root_topic = self.__get_root_topic__(self.sheet)
+        self.root_topic_title = self.root_topic['title']
+        self.catalog = self.generate_catalog(self.root_topic, [])
+        self.test_data = []
+        self.test_case = []
+        self.generate_test_data(self.catalog)
+
+    def get_sheet(self, index):
+        """
+        获取画布
+        :param index:
+        :return:
+        """
+        if isinstance(index, int):
+            sheet = self.content_json[index]
+        elif isinstance(index, str):
+            for sheet_content in self.content_json:
+                if sheet_content['title'] == index:
+                    sheet = sheet_content
+                    break
+            else:
+                raise Exception(f'未找到{index}画布(sheet)')
+        else:
+            sheet = {}
+            raise TypeError
+
+        self.sheet = sheet
+
+        return sheet
+
+    @staticmethod
+    def __get_root_topic__(sheet):
+        """
+        获取根话题
+        :param sheet: 画布
+        :return:
+        """
+        root_topic = sheet['rootTopic']
+
+        return root_topic
+
+    @staticmethod
+    def __get_title__(topic):
+        """
+        获取标题
+        :return:
+        """
+        title = topic['title']
+
+        return title
+
+    @staticmethod
+    def __get_notes__(topic):
+        """
+        获取笔记
+        :param topic:
+        :return:
+        """
+        notes = topic['notes']['plain']['content']
+
+        return notes
+
+    @staticmethod
+    def __get_labels__(topic):
+        """
+        获取标签
+        :param topic:
+        :return:
+        """
+        labels = topic['labels']
+
+        return labels
+
+    @staticmethod
+    def __get_callout__(topic):
+        """
+        获取标注
+        :param topic:
+        :return:
+        """
+        callout = topic['callout']['title']
+
+        return callout
+
+    @staticmethod
+    def __get_markers__(topic):
+        """
+        获取标记
+        :param topic:
+        :return:
+        """
+        markers = []
+        for mark in topic['markers']:
+            markers.append(mark['markerId'])
+
+        return markers
+
+    @staticmethod
+    def __get_summaries__(topic):
+        """
+        获取概要
+        :param topic:
+        :return:
+        """
+        summaries = []
+        for index in range(len(topic['summaries'])):
+            summary_range = eval(topic['summaries'][index]['range'])
+            summaries.append(
+                {'title': topic['children']['summary'][index]['title'],
+                 'range': summary_range,
+                 'start': summary_range[0],
+                 'len': summary_range[1] - summary_range[0]}
+            )
+
+        return summaries
+
+    @staticmethod
+    def __get_children__(topic):
+        """
+        获取子话题
+        :return:
+        """
+        children = topic['children']['attached']
+
+        return children
+
+    def generate_catalog(self, topic, catalog=None):
         """
         生成目录结构
-        :param content_json: Xmind原始JSON
+        :param topic: 目录
         :param catalog: 用于递归临时存储目录
         :return: 目录结构
         格式: JSON
-        备注: 原始JSON精简版, 仅保留3种标签:
+        备注: 原始JSON精简版, 保留标签如下:
         'title': 标题
         'notes': 笔记（前置条件）
-        'children': 子节点
+        'labels': 标签(简短描述)
+        'callout': 标注(待定)
+        ’markers': 标记（优先级） list
+        'summaries': 概要 list
+            title ---> 标题
+            range ---> 范围
+            start ---> 其实索引
+            len ---> 长度
+        'children': 子节点  list
         """
         catalog.append({})
-        for key in content_json:
+        for key in topic.keys():
             if key == 'title':
-                catalog[-1]['title'] = content_json[key]
+                catalog[-1]['title'] = self.__get_title__(topic)
             if key == 'notes':
-                catalog[-1]['notes'] = content_json['notes']['plain']['content']
+                catalog[-1]['notes'] = self.__get_notes__(topic)
+            if key == 'labels':
+                catalog[-1]['labels'] = self.__get_labels__(topic)
+            if key == 'callout':
+                catalog[-1]['callout'] = self.__get_callout__(topic)
+            if key == 'markers':
+                catalog[-1]['markers'] = self.__get_markers__(topic)
+            if key == 'summaries':
+                catalog[-1]['summaries'] = self.__get_summaries__(topic)
             if key == 'children':
                 catalog[-1]['children'] = []
-                for child in content_json['children']['attached']:
+                children = self.__get_children__(topic)
+                for child in children:
                     self.generate_catalog(child, catalog[-1]['children'])
 
         return catalog
@@ -46,71 +187,114 @@ class XmindReader:
         获取Xmind原始JSON
         :return:
         """
-        return self._content_json
+        return self.content_json
 
     def get_catalog(self):
         """
         获取目录结构
         :return: dict, {'一级目录1': {'二级目录1':{}, '二级目录2': {}}}
         """
-        return self._catalog
+        return self.catalog
 
     def get_test_data(self):
         """
         获取测试数据
         :return: list, 每个用例步骤之间以"-"分割
         """
-        return self._test_data
+        return self.test_data
 
     def get_first_title(self):
         """
         获取根目录标题
         :return:
         """
-        return self._catalog[0]['title']
+        return self.catalog[0]['title']
 
     def get_test_case(self):
-        for data in self._test_data:
-            self._test_case.append(data['test_case'])
+        for data in self.test_data:
+            self.test_case.append(data['test_case'])
 
-        return self._test_case
+        return self.test_case
 
-    def __generate_test_data__(self, catalog, test_case='', condition='', level=''):
+    def generate_test_data(self, catalog, test_case='', precondition='', label='', priority='', summaries=None):
         """
         生成测试用例
         :param catalog: 目录结构
         :param test_case: 递归临时存储测试用例
-        :param condition: 前置条件
-        :param level: 优先级
+        :param precondition: 前置条件
+        :param label: 标签
+        :param priority: 优先级
+        :param summaries: 概要
         :return:
         """
-        level_match = {'priority-1': '1', 'priority-2': '2', 'priority-3': '3', 'priority-4': '4'}
-        for topic in catalog:
-            tmp_condition = condition
+        priority_match = {'priority-1': 'High',
+                          'priority-2': 'Normal',
+                          'priority-3': 'Low'}
+
+        for index in range(len(catalog)):
+            topic = catalog[index]
+            summary = ''
+            if summaries:
+                # print(summaries)
+                for s in summaries:
+                    if index in range(s['range'][0], s['range'][1] + 1):
+
+                        summary = s['title']
+                        # print(summary)
+
             if 'notes' in topic.keys():
-                condition = topic['notes']
+                precondition = topic['notes']
+
+            if 'labels' in topic.keys():
+                label = '\n'.join(topic['labels'])
+
             if 'markers' in topic.keys():
-                if topic['markers'][0]['markerId'] in level_match.keys():
-                    level = level_match[topic['markers'][0]['markerId']]
+                for mark in topic['markers']:
+                    if mark in priority_match.keys():
+                        priority = priority_match[mark]
+                        break
+
+            if 'summaries' in topic.keys():
+                summaries = topic['summaries']
 
             if 'children' in topic.keys():
                 if test_case:
-                    self.__generate_test_data__(topic['children'], test_case + self.connector + topic['title'], condition, level=level)
+                    self.generate_test_data(catalog=topic['children'],
+                                            test_case=test_case + self.connector + topic['title'],
+                                            precondition=precondition,
+                                            label=label,
+                                            priority=priority,
+                                            summaries=summaries)
+                    # print(test_case)
                 else:
-                    self.__generate_test_data__(topic['children'], topic['title'], condition, level=level)
-            else:
-                test_data = {'condition': condition, 'test_case': test_case + self.connector + topic['title'], 'level': level}
-                self._test_data.append(test_data)
-                print(test_data)
+                    self.generate_test_data(catalog=topic['children'],
+                                            test_case=topic['title'],
+                                            precondition=precondition,
+                                            label=label,
+                                            priority=priority,
+                                            summaries=summaries)
 
-            condition = tmp_condition
+            else:
+                if summary:
+                    test_data = {'precondition': precondition,
+                                 'label': label,
+                                 'test_case': test_case + self.connector + topic['title'] + self.connector + summary,
+                                 'priority': priority}
+                else:
+                    test_data = {'precondition': precondition,
+                                 'label': label,
+                                 'test_case': test_case + self.connector + topic['title'],
+                                 'priority': priority}
+                # print(test_data)
+                self.test_data.append(test_data)
 
 
 if __name__ == '__main__':
-    path = './ZentaoCaseBuilder/Xmind/管理后台123.xmind'
+    path = 'D:\workspace\ZAGitlab\ZaTools\ZACaseBuilder\Xmind\调试脑图.xmind'
     xm = XmindReader(path)
+    # print(xm.content_json)
     print(xm.get_catalog())
     print(xm.get_test_data())
-    print(xm.get_test_case())
-    print(xm.get_first_title())
-
+    # print(xm.get_test_case())
+    # print(xm.get_first_title())
+    print(xm.root_topic_title)
